235a7cb2baa0e7d37959ae4beea777997a57be73
jonesjacklewis <jones.jack.lewis@gmail.com>
2025-02-26 12:42:48 +0000

    Added fix for -l and added word count -w

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"unicode"
 )
 
 func is_valid_switch(switch_chosen string) bool {
 	valid_switch_map := map[string]bool{
 		"-c": true,
 		"-l": true,
+		"-w": true,
 	}
 
 	return valid_switch_map[switch_chosen]
@@ -59,22 +61,88 @@ func handle_dash_l(os_args []string) (string, error) {
 	number_of_lines := 0
 
 	buf := make([]byte, 1024)
+	has_content := false
 
 	for {
 		n, err := file.Read(buf)
+
+		if n == 0 {
+			break
+		}
+
+		for i := 0; i < n; i++ {
+			if buf[i] == '\n' || buf[i] == '\r' {
+				number_of_lines++
+			} else {
+				if number_of_lines == 0 {
+					has_content = true
+				}
+			}
+		}
+
+		if err != nil {
+			break
+		}
+	}
+
+	if number_of_lines == 0 && has_content {
+		number_of_lines = 1
+	}
+
+	return fmt.Sprintf("%d %s", number_of_lines, filename), nil
+}
+
+func handle_dash_w(os_args []string) (string, error) {
+	if len(os_args) != 3 {
+		return "", fmt.Errorf("Invalid number of arguments")
+	}
+
+	filename := os_args[2]
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return "", fmt.Errorf("Error opening file: %s", filename)
+	}
+
+	defer file.Close()
+
+	number_of_words := 0
+
+	buf := make([]byte, 1024)
+	in_word := false
+
+	for {
+		n, err := file.Read(buf)
+
+		if n == 0 {
+			break
+		}
+
+		for i := 0; i < n; i++ {
+			at_position := buf[i]
+
+			if unicode.IsSpace(rune(at_position)) {
+				if in_word {
+					number_of_words++
+					in_word = false
+				}
+			} else {
+				in_word = true
+			}
+		}
 
 		if err != nil {
 			break
 		}
 
-		for i := 0; i < n; i++ {
-			if buf[i] == '\n' {
-				number_of_lines++
-			}
-		}
 	}
 
-	return fmt.Sprintf("%d %s", number_of_lines, filename), nil
+	if in_word {
+		number_of_words++
+	}
+
+	return fmt.Sprintf("%d %s", number_of_words, filename), nil
 }
 
 // func main() that takes command line arguments
@@ -107,6 +175,17 @@ func main() {
 		return
 	case "-l":
 		message, err := handle_dash_l(os.Args)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(message)
+
+		return
+	case "-w":
+		message, err := handle_dash_w(os.Args)
 
 		if err != nil {
 			fmt.Println(err)
--- main_test.go
+++ main_test.go
@@ -111,7 +111,6 @@ func Test_handle_dash_c(t *testing.T) {
 		}
 
 		expected_message := fmt.Sprintf("%d %s", len(content_to_write), filename)
-		fmt.Println(expected_message)
 
 		if message != expected_message {
 			t.Errorf("Expected %s got %s", expected_message, message)
@@ -221,4 +220,144 @@ func Test_handle_dash_l(t *testing.T) {
 		}
 
 	})
+
+	t.Run("File contains one line", func(t *testing.T) {
+		random_file_name := uuid.New().String()
+		filename := fmt.Sprintf("%s.txt", random_file_name)
+
+		// Create a file with the random_file_name
+		file, err := os.Create(filename)
+
+		if err != nil {
+			t.Errorf("Error creating file: %s", filename)
+		}
+
+		_, err = file.WriteString("Hello, World!")
+
+		if err != nil {
+			t.Errorf("Error writing to file: %s", filename)
+		}
+
+		file.Close()
+
+		os_args := []string{"gowc", "-l", filename}
+		message, err := handle_dash_l(os_args)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %s", err)
+		}
+
+		expected_message := fmt.Sprintf("1 %s", filename)
+
+		if message != expected_message {
+			t.Errorf("Expected %s got %s", expected_message, message)
+		}
+
+		// Remove the file
+		err = os.Remove(filename)
+
+		if err != nil {
+			t.Errorf("Error removing file: %s", filename)
+		}
+
+	})
+
+}
+
+func Test_handle_dash_w(t *testing.T) {
+	t.Run("Invalid number of arguments", func(t *testing.T) {
+		os_args := []string{"gowc", "-w"}
+		_, err := handle_dash_w(os_args)
+
+		if err == nil {
+			t.Errorf("Expected error got nil")
+		}
+	})
+
+	t.Run("File does not exist", func(t *testing.T) {
+		random_file_name := uuid.New().String()
+		os_args := []string{"gowc", "-w", fmt.Sprintf("%s.txt", random_file_name)}
+		_, err := handle_dash_l(os_args)
+
+		if err == nil {
+			t.Errorf("Expected error got nil")
+		}
+	})
+
+	t.Run("File is empty", func(t *testing.T) {
+		random_file_name := uuid.New().String()
+		filename := fmt.Sprintf("%s.txt", random_file_name)
+
+		// Create a file with the random_file_name
+		file, err := os.Create(filename)
+
+		if err != nil {
+			t.Errorf("Error creating file: %s", filename)
+		}
+
+		file.Close()
+
+		os_args := []string{"gowc", "-w", filename}
+		message, err := handle_dash_w(os_args)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %s", err)
+		}
+
+		expected_message := fmt.Sprintf("0 %s", filename)
+
+		if message != expected_message {
+			t.Errorf("Expected %s got %s", expected_message, message)
+		}
+
+		// Remove the file
+		err = os.Remove(filename)
+
+		if err != nil {
+			t.Errorf("Error removing file: %s", filename)
+		}
+
+	})
+
+	t.Run("File is not empty", func(t *testing.T) {
+		random_file_name := uuid.New().String()
+		filename := fmt.Sprintf("%s.txt", random_file_name)
+
+		// Create a file with the random_file_name
+		file, err := os.Create(filename)
+
+		if err != nil {
+			t.Errorf("Error creating file: %s", filename)
+		}
+
+		_, err = file.WriteString("Hello World")
+
+		if err != nil {
+			t.Errorf("Error writing to file: %s", filename)
+		}
+
+		file.Close()
+
+		os_args := []string{"gowc", "-w", filename}
+		message, err := handle_dash_w(os_args)
+
+		if err != nil {
+			t.Errorf("Expected no error, got %s", err)
+		}
+
+		expected_message := fmt.Sprintf("2 %s", filename)
+
+		if message != expected_message {
+			t.Errorf("Expected %s got %s", expected_message, message)
+		}
+
+		// Remove the file
+		err = os.Remove(filename)
+
+		if err != nil {
+			t.Errorf("Error removing file: %s", filename)
+		}
+
+	})
+
 }
